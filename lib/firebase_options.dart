1271b72c7316e3c48a7be38c470497b809a5cf8a
Mostak <mostakahmedshahad74@gmail.com>
2025-03-11 00:44:59 +0600

    edit_firebase_option

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,9 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 
 FirebaseOptions firebaseOptions = const FirebaseOptions(
   apiKey: "AIzaSyCfOjSyiECDn985s-WwDJiYc6vsuwd3F1M",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "1080757719252",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "lets-chat-e07a2",
+  authDomain: "lets-chat-e07a2.firebaseapp.com",
+  projectId: "lets-chat-e07a2",
+  storageBucket: "lets-chat-e07a2.firebasestorage.app",
+  messagingSenderId: "1080757719252",
+  appId: "1:1080757719252:web:303c9ff43d396323a96b41",
 );
